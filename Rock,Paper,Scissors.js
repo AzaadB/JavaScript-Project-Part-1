623ea7642fbcd5b6cdf4ca46faa689cfc8f5fdd9
AzaadB <azaadb09@gmail.com>
2022-09-09 14:58:34 +0200

    Update Rock,Paper,Scissors.js

--- Rock,Paper,Scissors.js
+++ Rock,Paper,Scissors.js
@@ -1,115 +1,111 @@
-console.log("Lets play a game!");
-
-function computerPlay() {
-  let playerOptions = ["Rock", "Paper", "Scissors"];
-  let randomString = Math.floor(Math.random() * playerOptions.length);
-  //Chooses an Playeroptions from the array on(line 2)//
-  return playerOptions[randomString];
-}
-
-function playRound(playerSelection, computerSelection) {
-  /*storing the message that will be outputed after,
-the round has finished and who the winner is in a variable called roundScore (line 13)*/
-  const roundResult = {
-    roundResultMessage: "",
-    roundWinner: "",
-  };
-  switch (playerSelection) {
-    /*In this first case(line 20) if statement, the computer and player both chose rock,
-    the message on (line 22) will be outputed*/
-    case "Rock":
-      if (computerSelection === "Rock"){
-        roundResult.roundResultMessage = `Tie! You both played ${playerSelection}`;
-      } else if (console.log(computerSelection === "Paper")) {
-        roundResult.roundResultMessage = `You lose! ${computerSelection} beats ${playerSelection}`;
-        roundResult.roundWinner = "computer";
-        /*In this else if statement(line 23) is saying if the computer had to choose paper,
-      the player would lose(line 24) and the computer will be the winner(line 25)*/
-      } else if (console.log(computerSelection === "Scissors")) {
-        roundResult.roundResultMessage = `You win! ${playerSelection} beats ${computerSelection}`;
-        roundResult.roundWinner = "player";
-        //In this else if statement(line 28) the winner will be the player due to rock beating scissors//
-      }
-      break;
-    case "Paper":
-      if (computerSelection === "Rock") {
-        roundResult.roundResultMessage = `You win! ${playerSelection} beats ${computerSelection}`;
-        roundResult.roundWinner = "player";
-        /*In this case(line 34) the option is "paper" in the if(line 35),
-        is saying if the computer chose rock and the player stuck with paper, 
-        the player will win the round(line 37)*/
-      } else if (console.log(computerSelection === "Paper")) {
-        roundResult.roundResultMessage = `Tie! You both played ${playerSelection}`;
-        /*In this else if statement(line 41), the computer and player both chose paper,
-        the message on (line 42) will outputed*/
-      } else if (console.log(computerSelection === "Scissors")) {
-        roundResult.roundResultMessage = `You lose! ${computerSelection} beats ${playerSelection}`;
-        roundResult.roundWinner = "computer";
-        /*In this else if statement(line 45), the computer chose scissors and the player chose paper, 
-        then the message on (line 46) will outputed and the computer will win the round(line 47)*/
-      }
-      break;
-    case "Scissors":
-      if (computerSelection === "Rock") {
-        roundResult.roundResultMessage = `You lose! ${computerSelection} beats ${playerSelection}`;
-        roundResult.roundWinner = "computer";
-        /*In this case(line 52) the option is "scissors" in the if(line 53),
-        is saying if the computer chose rock and the player stuck with scissors, 
-        the message on (line 54) will be outputed,
-        and player will lose the round and the computer will be the winner(line 55)*/
-      } else if (console.log(computerSelection === "Paper")) {
-        roundResult.roundResultMessage = `You win! ${playerSelection} beats ${computerSelection}`;
-        roundResult.roundWinner = "player";
-        /*In this else if statement(line 60), the computer chose rock and the player
-        chose scissors, the message on (line 61) will outputed and the player will win the round(line 62)*/
-      } else if (console.log(computerSelection === "Scissors")){
-        roundResult.roundResultMessage = `Tie! You both played ${playerSelection}`;
-        /*In this else if statement(line 63), the computer and player both
-        chose scissors, the message on (line 66) will outputed*/
-      }
-      break;
-    default:
-      console.log("You did not enter a valid option!");
+const getComputerChoice = () => {
+  let getNbr = Math.floor(Math.random()*2);
+  if (getNbr === 0) {
+    return 'rock';
+  } else if (getNbr === 1) {
+    return 'paper';
+  } else {
+    return 'scissors';
   }
-  return roundResult;
-}
+};
 
-function game() {
-  //creating a NEW function called game()(line 77)
-  let playerScore = 0,
-    computerScore = 0,
-    gameResultMessage;
-  for (let i = 0; i < 5; i++) {
-    let userSelection = prompt(
-      `Round ${i + 1}: Please enter rock, paper or scissors.`
-    );
-    let formattedUserSelection =
-      userSelection.charAt(0).toUpperCase() +
-      userSelection.slice(1).toLowerCase();
-    let { roundResultMessage, roundWinner } = playRound(
-      formattedUserSelection,
-      computerPlay()
-    );
-    if (roundResultMessage === "" && roundWinner === ""){
-      i--;
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return 'TIE here !';
+  }
+  if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'COMPUTER WON !';
     } else {
-      `Round ${i + 1}: ${roundResultMessage}`;
-      if (roundWinner === "player") {
-        playerScore += 1;
-      } else if (roundWinner === "computer") {
-        computerScore += 1;
-      }
+      return 'USER WON !';
     }
   }
-  if (playerScore === computerScore){
-    gameResultMessage = "The game was a tie";
-  } else {
-    gameResultMessage =
-      playerScore > computerScore
-        ? `\nCongratulations! You won the game! \nPlayer Score: ${playerScore} Computer Score: ${computerScore}`
-        : `\nSorry! You lost the game! \nPlayer Score: ${playerScore} Computer Score: ${computerScore}`;
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'COMPUTER WON !';
+    } else {
+      return 'USER WON !';
+    }
   }
-  console.log(`END OF THE GAME! ${gameResultMessage}`);
-}
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'COMPUTER WON !';
+    } else {
+      return 'USER WON';
+    }
+  }
+};
 
-game();
+const playGame = () => {
+  const userChoice = userInput;
+  const computerChoice = getComputerChoice();
+  console.log(`userChoice: ${userChoice} 
+   VS. computerChoice: ${computerChoice}`);
+  console.log(determineWinner(userChoice, computerChoice));  
+};
+
+playGame();
+
+/*
+Rock paper scissors is a classic two player game. Each player chooses either rock, paper, or scissors. The items are compared, and whichever player chooses the more powerful item wins.
+The possible outcomes are:
+Rock destroys scissors.
+Scissors cut paper.
+Paper covers rock.
+If there's a tie, then the game ends in a draw.
+Our code will break the game into four parts:
+Get the user's choice.
+Get the computer's choice.
+Compare the two choices and determine a winner.
+Start the program and display the results.
+Mark the tasks as complete by checking them off
+1.
+The user should be able to choose 'rock', 'paper', or 'scissors' when the game starts.
+Using const and arrow function syntax, create a function named getUserChoice that takes a single parameter userInput.
+2.
+Since a user can pass in a parameter, such as 'Rock' or 'rock' with different capitalizations, begin by utilizing JavaScript's toLowerCase() function to make the userInput all lowercase.
+You can use code like this:
+userInput = userInput.toLowerCase();
+3.
+When getting the user's choice, you should also check to make sure that the user typed a valid choice: 'rock', 'paper', or 'scissors'.
+Inside getUserChoice(), write an if/else statement that makes sure the userInput is either 'rock', 'paper', or 'scissors'. If it does, then return the userInput. If not, use console.log to print an error message to the console.
+4.
+Test the function by calling it with valid and invalid input, and printing the results to the console.
+You can delete this when you know your function works.
+5.
+Now we need to have the computer make a choice.
+Create a new function named getComputerChoice with no parameters. Inside its block, utilize Math.random() and Math.floor() to get a whole number between 0 and 2. Then, depending on the number, return either 'rock', 'paper', or 'scissors'.
+6.
+Test the function by calling it multiple times and printing the results to the console.
+You can delete this when you know your function works.
+7.
+Now it's time to determine a winner.
+Create a function named determineWinner that takes two parameters named userChoice and computerChoice. This function will compare the two choices played and then return if the human player won, lost, or tied.
+Let's deal with the tie condition first. Within the determineWinner() function, write an if statement that checks if the userChoice parameter equals the computerChoice parameter. If so, return a string that the game was a tie.
+8.
+If the game is not a tie, you'll need to determine a winner.
+Begin by writing an if statement that checks if the userChoice is 'rock'. Inside the if statement's block, write another if/else statement. The inner if/else should check if the computerChoice is 'paper'. If so, return a message that the computer won. If not, return a message that the user won.
+9.
+Next, write another if statement for if the userChoice is 'paper'.
+Inside this if statement, the computerChoice must be either 'scissors' or 'rock'. Write logic that will return a winner.
+10.
+Next, write yet another if statement for if the userChoice is 'scissors'.
+Inside of this if statement, the computerChoice must either be 'rock' or 'paper'. Write logic that will return a winner.
+11.
+Don't forget to test your function!
+Check off this task when you've finished testing.
+12.
+Everything is set up. Now you need to start the game and log the results.
+Create a function named playGame.
+Inside the playGame() function, create a variable named userChoice set equal to the result of calling getUserChoice(), passing in either 'rock', 'paper', or 'scissors' as an argument.
+Create another variable named computerChoice, and set it equal to the result of calling getComputerChoice().
+Under both of these variables, use console.log to print them to the console.
+13.
+Finally, let's determine who won.
+Inside the playGame() function, call the determineWinner() function. Pass in the userChoice and computerChoice variables as its parameters. Make sure to put this function call inside of a console.log() statement so you can see the result.
+Then, to start the game, call the playGame() function on the last line of your program.
+14.
+Make this game better by adding a secret cheat code. If a user types 'bomb' as their choice, then make sure they win, no matter what.
+15.
+Click here for a video walkthrough from our experts to help you check your work!
+*/
